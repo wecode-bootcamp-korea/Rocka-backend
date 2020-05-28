72028181d701ddad15ce0d61dfc4aaf51b0d3333
youngbinha <guseowhtjs@gmail.com>
2020-05-28 14:37:38 +0900

    [add] product앱 추가, member/models.py에 내용추가  - member/models.py 에 created_at과 updated_at을 추가,회원정보관리에필요  - product앱 models.py작성하기위해 만듬.

--- member/models.py
+++ member/models.py
@@ -7,6 +7,8 @@ class Member(models.Model):
     address			= models.CharField(max_length=500)
     password		= models.CharField(max_length=50)
     gender			= models.ForeignKey('Gender', on_delete = models.SET_NULL, null=True)
+    created_at      = models.DateTimeField(auto_now_add = True)
+    updated_at      = models.DateTimeField(auto_now = True)
 
     class Meta:
         db_table = 'members'
@@ -18,12 +20,12 @@ class Gender(models.Model):
         db_table = 'genders'
 
 class ShippingAddress(models.Model):
-    destination		= models.CharField(max_length=100)
-    receiver		= models.CharField(max_length=50)
-    address			= models.CharField(max_length=500)
-    phone_number	= models.CharField(max_length=50)
-    member			= models.ForeignKey(Member, on_delete = models.SET_NULL, null=True)
-    is_default		= models.BooleanField(default=False)
+    destination  = models.CharField(max_length=100)
+    receiver     = models.CharField(max_length=50)
+    address      = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=50)
+    member       = models.ForeignKey(Member, on_delete = models.SET_NULL, null=True)
+    is_default   = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'shipping_address'
