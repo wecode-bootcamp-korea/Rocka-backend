8ec3a0fd31d867ee90c3a173f973567d795f7dae
youngbinha <guseowhtjs@gmail.com>
2020-06-01 22:56:34 +0900

    [FIX]리뷰에서 수정권고된 내용 수정

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, CategoryView
 
 urlpatterns = [
     path('/main', ProductView.as_view()),
-    path('category',ProductView.as_view()),
+    path('category',CategoryView.as_view()),
 ]
