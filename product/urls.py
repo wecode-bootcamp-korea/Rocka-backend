3e5b245c4c24f46e020487d2ce12c426585acecc
youngbinha <guseowhtjs@gmail.com>
2020-06-01 22:51:34 +0900

    [FIX]리뷰에서 수정권고된 내용 수정

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from .views import ProductView
 
 urlpatterns = [
     path('/main', ProductView.as_view()),
+    path('category',ProductView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -5,28 +5,31 @@ from django.http    import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
-from .models    import Product
+from .models    import Product,Category
 
 class ProductView(View):
     def get(self, request):
         all_prod = Product.objects.all()
-        products= []
-        for prod in all_prod:
-            category            = [category.category.name for category in prod.categoryproduct_set.all()]
-            color_name_stock    = [{
+        products=[{
+            'id'                    : prod.id,
+            'name'                  : prod.name,
+            'price_krw'             : prod.price_krw,
+            'description'           : prod.description,
+            'category'              : [category.category.name for category in prod.categoryproduct_set.all()],
+            'color'                 : [{
                 'name'          : element.color.name,
                 'stock_quantity': element.stock_quantity,
                 'image_url'     : element.color.image_url
-            } for element in prod.colorproduct_set.all()]
-            products.append({
-                'id'                    : prod.id,
-                'name'                  : prod.name,
-                'price_krw'             : prod.price_krw,
-                'description'           : prod.description,
-                'category'              : category,
-                'color'                 : color_name_stock,
-                'outer_front_image_url' : prod.outer_front_image_url,
-                'outer_back_image_url'  : prod.outer_back_image_url,
-                'launchdate'            : prod.launch_date
-            })
+            } for element in prod.colorproduct_set.all()],
+            'outer_front_image_url' : prod.outer_front_image_url,
+            'outer_back_image_url'  : prod.outer_back_image_url,
+            'launchdate'            : prod.launch_date
+        } for prod in all_prod]
         return JsonResponse({'data':products}, status=200)
+
+
+class CategoryView(View):
+    def get(self, request):
+        all_cate = [cate.name for cate in Category.objects.all()]
+
+        return JsonResponse({'data':all_cate}, status=200)
