028dc847a2025a3f2f8c75cd3fb082693fc706a5
youngbinha <guseowhtjs@gmail.com>
2020-06-02 16:09:57 +0900

    [FIX]product/urls.py에서 url 고침

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductView, CategoryView
 
 urlpatterns = [
-    path('/main', ProductView.as_view()),
+    path('', ProductView.as_view()),
     path('/category',CategoryView.as_view()),
 ]
