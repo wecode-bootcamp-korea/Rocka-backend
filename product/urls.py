b6024872f409d91ca9d8b5f46f92beb1d24aec65
youngbinha <guseowhtjs@gmail.com>
2020-06-02 14:11:54 +0900

    [ADD]Product/views.py에 카테고리 view 수정

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,5 @@ from .views import ProductView, CategoryView
 
 urlpatterns = [
     path('/main', ProductView.as_view()),
-    path('/category',CategoryView.as_view()),
+    path('/category/<int:category_id>',CategoryView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -29,7 +29,27 @@ class ProductView(View):
 
 
 class CategoryView(View):
-    def get(self, request):
-        all_cate = [cate.name for cate in Category.objects.all()]
+    def get(self, request, category_id):
+        category = Category.objects.get(id=category_id)
+        products = category.categories.all()
+        try:
+            category_info = {'name' : category.name}
+            products_info = [{
+                'id'                    : prod.id,
+                'name'                  : prod.name,
+                'price_krw'             : prod.price_krw,
+                'description'           : prod.description,
+                'category'              : [category.category.name
+                                           for category in prod.categoryproduct_set.all()],
+                'color'                 : [{
+                    'name'          : element.color.name,
+                    'stock_quantity': element.stock_quantity,
+                    'image_url'     : element.color.image_url
+                } for element in prod.colorproduct_set.all()],
+                'outer_front_image_url' : prod.outer_front_image_url,
+                'outer_back_image_url'  : prod.outer_back_image_url,
+            } for prod in products]
 
-        return JsonResponse({'data':all_cate}, status=200)
+            return JsonResponse({'category':category_info,'product':products_info}, status=200)
+        except Category.DoesNotExist:
+            return JsonResponse({'message':'INVALID_CATEGHORY'}, status=400)
