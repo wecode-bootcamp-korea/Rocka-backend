fdf0234068437412c913a34dd118e462a2ebbfe4
youngbinha <guseowhtjs@gmail.com>
2020-06-03 15:28:18 +0900

    [FIX]product/views.py 불필요한 반복되는 코드 줄임

--- product/views.py
+++ product/views.py
@@ -12,68 +12,30 @@ class ProductView(View):
         all_prod    = Product.objects.all().prefetch_related('categoryproduct_set',
                                                           'colorproduct_set')
         query       = request.GET.get('category',all_prod)
-        try:
-            if query == all_prod:
-                products=[{
-                    'id'                    : prod.id,
-                    'name'                  : prod.name,
-                    'price_krw'             : prod.price_krw,
-                    'description'           : prod.description,
-                    'category'              :
-                    [category.category.name for category in prod.categoryproduct_set.all()],
-                    'color'                 : [{
-                        'name'          : element.color.name,
-                        'stock_quantity': element.stock_quantity,
-                        'image_url'     : element.color.image_url
-                    } for element in prod.colorproduct_set.all()],
-                    'outer_front_image_url' : prod.outer_front_image_url,
-                    'outer_back_image_url'  : prod.outer_back_image_url,
-                    'launchdate'            : prod.launch_date
-                } for prod in all_prod]
+        category_all= Category.objects.all().prefetch_related('product_set')
+        if query == all_prod:
+            prod_all = all_prod
+        else:
+            prod_all = category_all.get(name=query).product_set.all().prefetch_related('colorproduct_set')
 
-                return JsonResponse({'data':products}, status=200)
-            else:
-                category_all    = Category.objects.all().prefetch_related('product_set')
-                prod_all        = category_all.get(name=query).product_set.all().prefetch_related(
-                    'colorproduct_set')
-                if len(prod_all)>1:
-                    products=[{
-                        'id'                    : prod.id,
-                        'name'                  : prod.name,
-                        'price_krw'             : prod.price_krw,
-                        'description'           : prod.description,
-                        'category'              : category_all.get(name=query).name,
-                        'color'                 : [{
-                            'name'          : element.color.name,
-                            'stock_quantity': element.stock_quantity,
-                            'image_url'     : element.color.image_url
-                        } for element in prod.colorproduct_set.all()],
-                        'outer_front_image_url' : prod.outer_front_image_url,
-                        'outer_back_image_url'  : prod.outer_back_image_url,
-                        'launchdate'            : prod.launch_date
-                    } for prod in prod_all]
-                    return JsonResponse({'data':products}, status=200)
-                else:
-                    prod = prod_all.first()
-                    products = {
-                        'id'                    : prod.id,
-                        'name'                  : prod.name,
-                        'price_krw'             : prod.price_krw,
-                        'description'           : prod.description,
-                        'category'              : category_all.get(name=query).name,
-                        'color'                 : [{
-                            'name'          : element.color.name,
-                            'stock_quantity': element.stock_quantity,
-                            'image_url'     : element.color.image_url
-                        } for element in prod.colorproduct_set.all()],
-                        'outer_front_image_url' : prod.outer_front_image_url,
-                        'outer_back_image_url'  : prod.outer_back_image_url,
-                        'launchdate'            : prod.launch_date
-                    }
-                    return JsonResponse({'data':products},status=200)
-        except KeyError:
-            return JsonResponse({'message':'INVALID_KEY'},status=400)
+        products=[{
+            'id'                    : prod.id,
+            'name'                  : prod.name,
+            'price_krw'             : prod.price_krw,
+            'description'           : prod.description,
+            'category'              :
+            [category.category.name for category in prod.categoryproduct_set.all()],
+            'color'                 : [{
+                'name'          : element.color.name,
+                'stock_quantity': element.stock_quantity,
+                'image_url'     : element.color.image_url
+            } for element in prod.colorproduct_set.all()],
+            'outer_front_image_url' : prod.outer_front_image_url,
+            'outer_back_image_url'  : prod.outer_back_image_url,
+            'launchdate'            : prod.launch_date
+        } for prod in prod_all]
 
+        return JsonResponse({'data':products}, status=200)
 
 class CategoryView(View):
     def get(self, request):
