1e552962e2497db20ea0014953cbf4cf31827b53
youngbinha <guseowhtjs@gmail.com>
2020-06-03 14:38:32 +0900

    [FIX]url로 카테고리 분류하는 것 views.py에 작성

--- product/views.py
+++ product/views.py
@@ -9,46 +9,91 @@ from .models    import Product,Category,Color
 
 class ProductView(View):
     def get(self, request):
-        all_prod = Product.objects.all().prefetch_related('categoryproduct_set',
+        all_prod    = Product.objects.all().prefetch_related('categoryproduct_set',
                                                           'colorproduct_set')
-        products=[{
-            'id'                    : prod.id,
-            'name'                  : prod.name,
-            'price_krw'             : prod.price_krw,
-            'description'           : prod.description,
-            'category'              : [category.category.name for category in prod.categoryproduct_set.all()],
-            'color'                 : [{
-                'name'          : element.color.name,
-                'stock_quantity': element.stock_quantity,
-                'image_url'     : element.color.image_url
-            } for element in prod.colorproduct_set.all()],
-            'outer_front_image_url' : prod.outer_front_image_url,
-            'outer_back_image_url'  : prod.outer_back_image_url,
-            'launchdate'            : prod.launch_date
-        } for prod in all_prod]
-        return JsonResponse({'data':products}, status=200)
+        query       = request.GET.get('category',all_prod)
+        try:
+            if query == all_prod:
+                products=[{
+                    'id'                    : prod.id,
+                    'name'                  : prod.name,
+                    'price_krw'             : prod.price_krw,
+                    'description'           : prod.description,
+                    'category'              :
+                    [category.category.name for category in prod.categoryproduct_set.all()],
+                    'color'                 : [{
+                        'name'          : element.color.name,
+                        'stock_quantity': element.stock_quantity,
+                        'image_url'     : element.color.image_url
+                    } for element in prod.colorproduct_set.all()],
+                    'outer_front_image_url' : prod.outer_front_image_url,
+                    'outer_back_image_url'  : prod.outer_back_image_url,
+                    'launchdate'            : prod.launch_date
+                } for prod in all_prod]
+
+                return JsonResponse({'data':products}, status=200)
+            else:
+                category_all    = Category.objects.all().prefetch_related('product_set')
+                prod_all        = category_all.get(name=query).product_set.all().prefetch_related(
+                    'colorproduct_set')
+                if len(prod_all)>1:
+                    products=[{
+                        'id'                    : prod.id,
+                        'name'                  : prod.name,
+                        'price_krw'             : prod.price_krw,
+                        'description'           : prod.description,
+                        'category'              : category_all.get(name=query).name,
+                        'color'                 : [{
+                            'name'          : element.color.name,
+                            'stock_quantity': element.stock_quantity,
+                            'image_url'     : element.color.image_url
+                        } for element in prod.colorproduct_set.all()],
+                        'outer_front_image_url' : prod.outer_front_image_url,
+                        'outer_back_image_url'  : prod.outer_back_image_url,
+                        'launchdate'            : prod.launch_date
+                    } for prod in prod_all]
+                    return JsonResponse({'data':products}, status=200)
+                else:
+                    prod = prod_all.first()
+                    products = {
+                        'id'                    : prod.id,
+                        'name'                  : prod.name,
+                        'price_krw'             : prod.price_krw,
+                        'description'           : prod.description,
+                        'category'              : category_all.get(name=query).name,
+                        'color'                 : [{
+                            'name'          : element.color.name,
+                            'stock_quantity': element.stock_quantity,
+                            'image_url'     : element.color.image_url
+                        } for element in prod.colorproduct_set.all()],
+                        'outer_front_image_url' : prod.outer_front_image_url,
+                        'outer_back_image_url'  : prod.outer_back_image_url,
+                        'launchdate'            : prod.launch_date
+                    }
+                    return JsonResponse({'data':products},status=200)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEY'},status=400)
 
 
 class CategoryView(View):
     def get(self, request):
-        all_category = Category.objects.all().prefetch_related('product_set')
+        all_category    = Category.objects.all().prefetch_related('product_set')
         category_info = [{
             'category_id'   : category.id,
             'name'          : category.name,
             'count'         : len(category.product_set.all())
-        } for category in all_category]
+            } for category in all_category]
         return JsonResponse({'data':category_info}, status=200)
 
 class DetailView(View):
     def get(self, request,product_id):
-        product         = Product.objects.get(id=product_id).selected_related(
-            'basic_information',
-            'color',
-            'manufacturer'
-        )
+        product         = Product.objects.select_related('basic_information').prefetch_related(
+            'color','manufacturer'
+        ).get(id=product_id)
         basic_info      = product.basic_information
         color_info      = product.color.all()
         manufac_info    = product.manufacturer.all()
+        order_quantity  = 0
         product_info    = {
             'id'                : product_id,
             'name'              : product.name,
@@ -64,6 +109,7 @@ class DetailView(View):
             'mfds'              : product.mfds,
             'how_to_use'        : product.how_to_use,
             'inner_image_url'   : product.inner_image_url,
+            'order_quantity'    : order_quantity,
             'manufacturer'      : [{
                 'name'          : manufac.name,
                 'country_name'  : manufac.country_name
