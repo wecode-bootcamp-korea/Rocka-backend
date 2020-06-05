183ec6af481030e234060732a444451db3306343
TeddyJung <teddyjung@TeddyJungui-MacBookPro.local>
2020-06-05 14:39:38 +0900

    [FIX] order app 내 카트 post get 코드 작성 - 주문 기능 (post) - 카트 정보 가저오기 (get)

--- order/views.py
+++ order/views.py
@@ -17,9 +17,7 @@ class CartView(View):
     def post(self, request):
         try:
             data                    = json.loads(request.body)
-            token                   = request.headers.get("Authorization",None)
-            token_payload           = jwt.decode(token, SECRET_KEY, algorithms="HS256")
-            nickname                = Member.objects.get(nickname=token_payload['nickname'])
+            nickname                = request.user
             now                     = datetime.now()
             ordernumber             = str(nickname.id) + "now.year" + "now.month" + "now.day" + "now.hour" + "now.minute"
             ordered_color_name      = data['name']
@@ -28,8 +26,7 @@ class CartView(View):
             if Order.objects.filter(member_id=nickname.id, order_status=1).exists():
                 order = Order.objects.filter(order_status=1).last()
 
-            else:
-                order = Order.objects.create(
+            order = Order.objects.create(
                     order_num       = ordernumber,
                     order_status    = OrderStatus.objects.get(id=1),
                     member          = nickname
@@ -39,13 +36,31 @@ class CartView(View):
             if prod.exists():
                 prod.filter(color__name=data['name']).update(quantity=ordered_quantity)
 
-            else:
-                OrderDetail.objects.create(
-                    product_id      = data['id'],
-                    quantity        = ordered_quantity,
+            OrderDetail.objects.create(
+                    product_id      = Product.objects.get(id=data['id']).id,
+                    quantity     = ordered_quantity,
                     color_id        = Color.objects.get(name=ordered_color_name).id,
                     order_id        = order.id
                 )
-
+            return HttpResponse(status=200)
         except KeyError:
             return HttpResponse(status=400)
+
+    @LoginConfirm
+    def get(self, request):
+        try:
+            user = request.user
+            cart_items = Order.objects.get(member=user, order_status=1).orderdetail_set.all().select_related('product','color')
+
+            mycart=[{
+                'name'                  : item.product.name,
+                'outer_front_image_url' : item.product.outer_front_image_url,
+                'order_quantity'        : item.quantity,
+                'price_krw'             : item.product.price_krw,
+                'color_name'            : item.color.name
+            } for item in cart_items]
+
+            return JsonResponse({'data':mycart}, status=200)
+
+        except:
+            return HttpResponse(status=401)
